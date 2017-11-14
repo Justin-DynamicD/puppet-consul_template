dbd75b4fe72e78f8ee70832a3ced6650c44c04ae
Justin King <jking@lanterncredit.com>
2017-11-13 22:43:06 -0800

    remove excess "/"

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,7 +10,7 @@ class consul_template::params {
   $package_name       = 'consul-template'
   $package_ensure     = 'latest'
   $version            = '0.19.3'
-  $download_url_base  = 'https://releases.hashicorp.com/consul-template/'
+  $download_url_base  = 'https://releases.hashicorp.com/consul-template'
   $download_checksum  = '47b3f134144b3f2c6c1d4c498124af3c4f1a4767986d71edfda694f822eb7680'
   $download_type      = 'sha256'
   $download_extension = 'zip'
